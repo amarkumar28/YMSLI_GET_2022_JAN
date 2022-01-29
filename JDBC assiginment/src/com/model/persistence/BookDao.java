43069c38191d3a9993a12fcf23a9badf971f59d2
a28k12 <ak5469553@gmail.com>
2022-01-29 16:55:33 +0530

    jdbc completed

---
+++ JDBC assiginment/src/com/model/persistence/BookDao.java
@@ -0,0 +1,11 @@
+package com.model.persistence;
+
+import java.util.List;
+
+public interface BookDao {
+	public List<Book> getAllBooks();
+	public Book getBookById(int bookId);
+	public void addBook(Book book);
+	public void updateBook(int bookId,Book book);
+	public void removeBook(int bookId);
+}
